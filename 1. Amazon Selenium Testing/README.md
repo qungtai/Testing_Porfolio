2531a6d9108a140f96d2e9639e5c368d82c615a8
qungtai <138693151+qungtai@users.noreply.github.com>
2023-10-04 20:00:31 +0700

    update

---
+++ 1. Amazon Selenium Testing/README.md
@@ -0,0 +1,14 @@
+# Testing_Porfolio
+This is a Amazon web testing. It's include:
+- Page load test
+- Item search test
+- Sign-in button click test
+- Sign-up button click test
+
+## Run
+To run this project: 
+
+- Make sure selenium package is installed in your python. If not, run this command:
+```pip install selenium```
+- To run all the test case: 
+```python -m unittest```
--- 1. Amazon Selenium Testing/test_cases.py
+++ 1. Amazon Selenium Testing/test_cases.py
@@ -5,7 +5,7 @@ def test_cases(number):
 testCases = [
     # [severity, description]
     ['Blocker', 'when user goes to main page, page should be loaded'],
-    ['Blocker', 'In Main page, when user search "Nexus 5" button, he should see result for Nexus 5'],
+    ['Blocker', 'In Main page, when user search "iPhone" button, he should see result for iPhone'],
     ['Blocker', 'In Main page, when user click "Sign-up" button, he should see Sign up Page'],
     ['Blocker', 'In Main page, when user click "Sign-in" button, he should see Sign in Page'],
 ]
